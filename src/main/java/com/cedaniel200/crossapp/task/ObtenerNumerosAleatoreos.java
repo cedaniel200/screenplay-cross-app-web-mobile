3e23dfbf57ddfae819f71f8a6fb04f03a987ea0b
cedaniel200 <cdanielmg200@gmail.com>
2020-05-17 16:44:28 -0500

    Se construye la tarea que obtendrá los números aleatoreos

---
+++ src/main/java/com/cedaniel200/crossapp/task/ObtenerNumerosAleatoreos.java
@@ -0,0 +1,46 @@
+package com.cedaniel200.crossapp.task;
+
+import com.cedaniel200.crossapp.userinterface.GooglePage;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.Open;
+import org.openqa.selenium.Keys;
+
+import static com.cedaniel200.crossapp.userinterface.GooglePage.CAMPO_DE_BUSQUEDA;
+import static com.cedaniel200.crossapp.userinterface.NumeroAleatoreoPage.*;
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static org.openqa.selenium.Keys.ENTER;
+
+public class ObtenerNumerosAleatoreos implements Task {
+
+    public static final String NUMERO_1 = "numero1";
+    public static final String NUMERO_2 = "numero2";
+
+    private GooglePage googlePage;
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn(googlePage),
+                Enter.theValue("numeros aleatorios")
+                        .into(CAMPO_DE_BUSQUEDA)
+                        .thenHit(ENTER),
+                Enter.theValue("0").into(NUMERO_MINIMO),
+                Enter.theValue("9").into(NUMERO_MAXIMO),
+                Click.on(GENERAR_NUMERO_ALEATOREO)
+        );
+        String numeroAleatoreo = NUMERO_ALEATOREO.resolveFor(actor).getText();
+        actor.remember(NUMERO_1, numeroAleatoreo);
+        actor.attemptsTo(
+                Click.on(GENERAR_NUMERO_ALEATOREO)
+        );
+        numeroAleatoreo = NUMERO_ALEATOREO.resolveFor(actor).getText();
+        actor.remember(NUMERO_2, numeroAleatoreo);
+    }
+
+    public static ObtenerNumerosAleatoreos obtenerNumerosAleatoreos() {
+        return instrumented(ObtenerNumerosAleatoreos.class);
+    }
+}
