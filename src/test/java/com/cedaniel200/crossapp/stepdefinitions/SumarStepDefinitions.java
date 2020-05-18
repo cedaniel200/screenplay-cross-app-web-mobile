4ac1b04357fbe5bd46936523fc1f06e0017f11b0
cedaniel200 <cdanielmg200@gmail.com>
2020-05-17 19:09:34 -0500

    Se implementan los médotos que representan el When y el Then

--- src/test/java/com/cedaniel200/crossapp/stepdefinitions/SumarStepDefinitions.java
+++ src/test/java/com/cedaniel200/crossapp/stepdefinitions/SumarStepDefinitions.java
@@ -1,5 +1,6 @@
 package com.cedaniel200.crossapp.stepdefinitions;
 
+import com.cedaniel200.crossapp.questions.Resultado;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -9,13 +10,17 @@ import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import org.openqa.selenium.WebDriver;
-import sun.security.util.PendingException;
+
+import java.net.MalformedURLException;
 
 import static com.cedaniel200.crossapp.task.ObtenerNumerosAleatoreos.obtenerNumerosAleatoreos;
+import static com.cedaniel200.crossapp.task.SumarNumerosAleatoreos.sumarNumerosAleatoreos;
 import static com.cedaniel200.crossapp.util.WebDriverFactory.getAppiumDriver;
 import static com.cedaniel200.crossapp.util.WebDriverFactory.getWebDriver;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+import static org.hamcrest.core.Is.is;
 
 public class SumarStepDefinitions {
 
@@ -23,9 +28,9 @@ public class SumarStepDefinitions {
     private WebDriver phone;
 
     @Before
-    public void prepararEscenario() {
-        chrome = getWebDriver();
+    public void prepararEscenario() throws MalformedURLException {
         phone = getAppiumDriver();
+        chrome = getWebDriver();
         OnStage.setTheStage(new OnlineCast());
     }
 
@@ -40,12 +45,14 @@ public class SumarStepDefinitions {
     @When("^[a-zA-Z]{3,40} suma dos numeros$")
     public void sumarDosNumeros() {
         theActorInTheSpotlight().can(BrowseTheWeb.with(phone));
+        theActorInTheSpotlight().attemptsTo(
+                sumarNumerosAleatoreos()
+        );
     }
 
     @Then("^[a-zA-Z]{3,40} deberia ver que su resultado es correcto$")
     public void verificarResultadoSuma() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        theActorInTheSpotlight().should(seeThat(Resultado.esCorrecto()));
     }
 
     @After
@@ -56,7 +63,6 @@ public class SumarStepDefinitions {
 
     private void cerrar(WebDriver driver) {
         if(driver != null){
-            driver.close();
             driver.quit();
         }
     }
