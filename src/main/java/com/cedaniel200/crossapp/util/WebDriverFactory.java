6a2addb1c293f4f0c2e5e7ac13d74a64040c19a1
cedaniel200 <cdanielmg200@gmail.com>
2020-05-17 19:04:01 -0500

    Se implementa el médoto que devuelve el appium driver para Android

--- src/main/java/com/cedaniel200/crossapp/util/WebDriverFactory.java
+++ src/main/java/com/cedaniel200/crossapp/util/WebDriverFactory.java
@@ -1,7 +1,13 @@
 package com.cedaniel200.crossapp.util;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverFactory {
 
@@ -9,8 +15,16 @@ public class WebDriverFactory {
         return new ChromeDriver();
     }
 
-    public static WebDriver getAppiumDriver(){
-        // TODO por implementar
-        return null;
+    public static WebDriver getAppiumDriver() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "TU_DISPOSITIVO"); // TODO agregar el valor
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        capabilities.setCapability("appPackage", "com.miui.calculator");
+        capabilities.setCapability("appActivity", "com.miui.calculator.cal.CalculatorActivity");
+        return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 }
